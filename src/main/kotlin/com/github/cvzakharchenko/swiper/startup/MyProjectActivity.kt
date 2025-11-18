72c17e6d3c7bb7b25ce21e2098c5d0e0f42359c5
GitHub Action <action@github.com>
2025-11-18 05:04:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/cvzakharchenko/swiper/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/cvzakharchenko/swiper/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.cvzakharchenko.swiper.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
